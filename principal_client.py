7784f0682ddfc1ae66b81f7b13fec5ace9bb6dd3
Cesc Blanco <cesc.blanco98@gmail.com>
2026-04-14 12:18:48 +0200

    Definition of the primary client

---
+++ principal_client.py
@@ -0,0 +1,13 @@
+import requests
+
+from providers.three_sixty_five_scores.client import Scores365Client
+from providers.transfermarkt.client import TransfermarktClient
+
+
+class FootballClient:
+    def __init__(self):
+        self.session = requests.Session()
+
+        # Providers
+        self.three_sixty_five_scores = Scores365Client(self.session)
+        self.transfermarkt = TransfermarktClient(self.session)
