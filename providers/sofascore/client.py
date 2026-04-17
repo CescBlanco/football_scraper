7d27e8d286b55743609835d3812da5bad2edf95f
Cesc Blanco <cesc.blanco98@gmail.com>
2026-04-17 12:10:17 +0200

    Updated functions team sofascore

--- providers/sofascore/client.py
+++ providers/sofascore/client.py
@@ -1,7 +1,7 @@
 import requests 
 # from .competitions import Scores365CompetitionService
 # from .league import Scores365LeagueScraper
-# from .team import Scores365TeamScraper
+from .team import SofascoreTeamScraper
 from .player import SofascorePlayerScraper
 # from .match import Scores365MatchScraper
 from .manager import SofascoreManagerScraper
@@ -18,7 +18,7 @@ class SofascoreClient:
         # Servicios
         # self.competition = Scores365CompetitionService(self.session)
         # self.league= Scores365LeagueScraper(self.session, self.competition)
-        # self.team= Scores365TeamScraper(self.session, self.competition)
+        self.team= SofascoreTeamScraper(self.session)
         self.player= SofascorePlayerScraper(self.session)
         # self.match= Scores365MatchScraper(self.session)
         self.manager= SofascoreManagerScraper(self.session)
